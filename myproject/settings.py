37d09025938d7709b8cae9d5314d7c1e09e555a5
unknown <TMCreme>
2018-09-05 18:06:38 +0000

    changed settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -130,13 +130,39 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+"""
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-"""
+
 MEDIA_URL = 'https://s3.us-east-2.amazonaws.com/dziedzormdelasi/'
 
 MEDIA_ROOT = "https://s3.us-east-2.amazonaws.com/dziedzormdelasi/")
 """
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'mysite/static'),
+]
+
+AWS_ACCESS_KEY_ID = 'AKIAJIUBN2QO3N6J3QIQ'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'brightonplace'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
+AWS_LOCATION = 'static'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+
+DEFAULT_FILE_STORAGE = 'mysite.storage_backends.MediaStorage'
+
+
+
+
+
