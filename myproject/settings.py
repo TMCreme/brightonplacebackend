e8d314ba8ca33d0fd376aa09a45f94ed9184b896
unknown <TMCreme>
2018-09-05 17:28:35 +0000

    added changed settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -129,17 +129,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-"""
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-
+"""
 MEDIA_URL = 'https://s3.us-east-2.amazonaws.com/dziedzormdelasi/'
 
 MEDIA_ROOT = "https://s3.us-east-2.amazonaws.com/dziedzormdelasi/")
-"""
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'mysite/static'),
 ]
@@ -156,7 +156,7 @@ AWS_S3_OBJECT_PARAMETERS = {
 AWS_LOCATION = 'static'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-
+"""
 
 DEFAULT_FILE_STORAGE = 'mysite.storage_backends.MediaStorage'
 
