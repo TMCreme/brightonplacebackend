77cd6dd2da7126fec31b40b288b09e0d2cb6748a
unknown <TMCreme>
2018-09-05 18:50:03 +0000

    changed settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -160,7 +160,7 @@ AWS_LOCATION = 'static'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
-DEFAULT_FILE_STORAGE = 'mysite.storage_backends.MediaStorage'
+DEFAULT_FILE_STORAGE = 'myproject.storage_backends.MediaStorage'
 
 
 
